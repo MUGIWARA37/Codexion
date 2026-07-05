3eec7f62e1490fce52886d6d19471cc4fb17f483
Rida Hlou <rhlou@e1r6p3.1337.ma>
2026-07-05 12:02:28 +0100

    file structure and makefile

--- Makefile
+++ Makefile
@@ -1,1 +1,30 @@
+NAME     = codexion
+CC       = cc
+CFLAGS   = -Wall -Wextra -Werror -pthread
+INCLUDES = -I include/
 
+SRCS     = src/main.c src/init.c src/coder.c src/dongle.c \
+           src/scheduler.c src/monitor.c src/log.c src/utils.c
+
+HEADER   = include/codexion.h
+
+OBJS     = $(patsubst src/%.c, objects/%.o, $(SRCS))
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+objects/%.o: src/%.c $(HEADER)
+	mkdir -p objects
+	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+clean:
+	rm -rf objects
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- include/codexion.h
+++ include/codexion.h
@@ -5,12 +5,74 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhlou <rhlou@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/02 15:37:34 by rhlou             #+#    #+#             */
-/*   Updated: 2026/07/03 10:29:00 by rhlou            ###   ########.fr       */
+/*   Created: 2026/07/03 17:32:43 by rhlou             #+#    #+#             */
+/*   Updated: 2026/07/03 18:50:47 by rhlou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CODEXION_H
-# define CODEXION_H
+#define CODEXION_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+typedef struct t_coder t_coder;
+
+typedef struct t_heap_entry
+{
+    long long priority;
+    int coder_id;
+}             t_heap_entry;
+
+typedef struct t_heap
+{
+    t_heap_entry* data;
+    int           size;
+    int           capacity;
+}              t_heap;
+
+typedef struct t_dongle
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    int is_available;
+    long long released_at;
+    t_heap  wait_queue;
+}              t_dongle;
+
+typedef struct  t_sim
+{
+    int num_coders;
+    long long  time_to_burnout;
+    long long  time_to_compile;
+    long long  time_to_debug;
+    long long  time_to_refactor;
+    int        num_compiles_required;
+    long long  dongle_cooldown;
+    int        use_edf;
+    t_coder*   coders;
+    t_dongle*  dongles;
+    pthread_mutex_t log_mutex;
+    pthread_mutex_t stop_mutex;
+    int             simulation_over;
+    long long  start_time;
+    long long fifo_counter;
+    pthread_mutex_t fifo_mutex;
+    pthread_t       monitor_thread;
+}               t_sim;
+
+
+
+typedef struct t_coder
+{
+    int        id;
+    pthread_t   thread;
+    long long  last_compile_start;
+    int compile_count;
+    t_sim*  sim;
+}              t_coder;
 
 #endif
