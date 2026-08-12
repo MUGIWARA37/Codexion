47d24fccefd22f9273335d4572e137b9207719ec
Rida Hlou <rhlou@e3r5p3.1337.ma>
2026-08-12 16:27:29 +0100

    final

--- src/utils2.c
+++ src/utils2.c
@@ -16,19 +16,25 @@ int	is_valid_number(const char *str)
 {
 	int			i;
 	long long	res;
+	int			sign;
 
 	i = 0;
 	res = 0;
+	sign = 1;
 	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
 		i++;
-	if (!str[i] || str[0] == '-')
+	}
+	if (!str[i])
 		return (0);
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
 			return (0);
 		res = res * 10 + (str[i] - '0');
-		if (res > 2147483647)
+		if (res * sign > 2147483647 || res * sign < -2147483648)
 			return (0);
 		i++;
 	}
